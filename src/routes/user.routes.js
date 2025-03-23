afb1735b250e1d0433bc935b769d525a90f4bb20
Vivek Kantipudi <vivekkantipudi1794@gmail.com>
2025-03-23 10:16:06 +0530

    bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js"
+import { logoutUser } from "../controllers/user.controller.js";
+import { loginUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
